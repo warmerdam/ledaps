235ee20de29789bab7da2fc1b629d4bc128fcae4
Frank Warmerdam <warmerdam@pobox.com>
2012-11-01 17:51:16 -0700

    refine check pixels

--- test/LE70120112001241PFS01/run_full.sh
+++ test/LE70120112001241PFS01/run_full.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 lndsr lndsr.L71012011_01120010829.txt \
-	-check 2450 850 \
-	-check 2460 860 \
+	-check 2450 850  \
+	-check 2460 860  \
 	-check 2800 1000 \
-        -checkll 70.45 -54.9325 \
+        -check 3050 970  \
+        -checkll 70.46175 -54.99569 \
 	|& tee full/lndsr.log
 
--- test/LE70120112001241PFS01/run_subarea.sh
+++ test/LE70120112001241PFS01/run_subarea.sh
@@ -2,8 +2,9 @@
 
 lndsr lndsr.L71012011_01120010829.txt \
 	-srcwin 2400 800 800 400 \
-	-check 2450 850 \
-	-check 2460 860 \
+	-check 2450 850  \
+	-check 2460 860  \
 	-check 2800 1000 \
-        -checkll 70.45 -54.9325 \
+        -check 3050 970  \
+        -checkll 70.46175 -54.99569 \
 	|& tee subarea/lndsr.log
