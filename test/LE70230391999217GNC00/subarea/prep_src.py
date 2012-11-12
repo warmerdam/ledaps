0f4ae7afd1ff090c379b632f77f34fca532be4f7
Frank Warmerdam <warmerdam@pobox.com>
2012-11-11 21:11:57 -0800

    air at landsat res, add thermal, lndcal_QA

--- test/LE70230391999217GNC00/subarea/prep_src.py
+++ test/LE70230391999217GNC00/subarea/prep_src.py
@@ -20,6 +20,19 @@ def extract_cal_bands(srcwin, src_hdf_file):
     for band in bands:
         extract_subwin(src + band, 'lndcal_band'+band, srcwin, 'Int16')
 
+def extract_lndcal_QA(srcwin, src_hdf_file, srs, gt):
+    srcfile = 'HDF4_SDS:UNKNOWN:"'+src_hdf_file+'":6'
+    cmd = "gdal_translate -srcwin %d %d %d %d -ot %s '%s' %s -of PAux -a_srs %s -a_ullr %.2f %.2f %.2f %.2f" \
+          % (srcwin[0], srcwin[1], srcwin[2], srcwin[3],
+             'Byte', srcfile, 'lndcal_QA', srs,
+             gt[0], gt[3],
+             gt[0] + srcwin[2] * gt[1],
+             gt[3] + srcwin[3] * gt[5])
+    print cmd
+    os.system(cmd)
+
+    
+
 def extract_cal_info(src_hdf_file):
     src = 'HDF4_EOS:EOS_GRID:"'+src_hdf_file+'":Grid:band1'
     ds = gdal.Open(src)
@@ -59,21 +72,33 @@ def write_parms(filename, parm_list):
 
 ar_subsample = 40
 src_cal='../lndcal.L71023039_03919990805.hdf'
-
+src_th='../lndth.L71023039_03919990805.hdf'
 srcwin=(2400,800,800,400)
 
 (full_input_size, srs, full_gt) = extract_cal_info(src_cal)
 
-utm_zone = int(srs[-2:])
-ul_x = full_gt[0] + srcwin[0] * full_gt[1]
-ul_y = full_gt[3] + srcwin[1] * full_gt[5]
-pixel_size = full_gt[1]
+win_gt = (full_gt[0] + srcwin[0] * full_gt[1],
+          full_gt[1],
+          full_gt[2],
+          full_gt[3] + srcwin[1] * full_gt[5],
+          full_gt[4],
+          full_gt[5])
 
-# TODO(warmerdam): make parametric
-aerosol_geotransform = (579285, 1200, 0,
-                        3435015, 0, -1200)
+utm_zone = int(srs[-2:])
+ul_x = win_gt[0]
+ul_y = win_gt[3]
+pixel_size = win_gt[1]
+
+aerosol_geotransform = (win_gt[0],
+                        win_gt[1]*ar_subsample,
+                        win_gt[2]*ar_subsample,
+                        win_gt[3],
+                        win_gt[4]*ar_subsample,
+                        win_gt[5]*ar_subsample)
 
 extract_cal_bands(srcwin, src_cal)
+extract_lndcal_QA(srcwin, src_cal, srs, win_gt)
+extract_subwin(src_th, 'thermal', srcwin, 'Int16')
 
 aerosol_size = (srcwin[2]/ar_subsample, srcwin[3]/ar_subsample)
 
@@ -84,14 +109,14 @@ extract_anc( srs, aerosol_geotransform, aerosol_size,
              '../anc/REANALYSIS_1999217_slp.tif',
              'slp')
 extract_anc( srs, aerosol_geotransform, aerosol_size,
-             '../anc/REANALYSIS_1999217_air.tif',
-             'air')
-extract_anc( srs, aerosol_geotransform, aerosol_size,
              '../anc/TOMS_1999217.tif',
              'ozone')
 extract_anc( srs, aerosol_geotransform, aerosol_size,
              '../anc/CMGDEM.tif',
              'dem')
+extract_anc( srs, win_gt, (srcwin[2], srcwin[3]),
+             '../anc/REANALYSIS_1999217_air.tif',
+             'air')
 
 extract_subwin('../AEROSOL_RUN_RESULTS.hdf', 'ar',
                (srcwin[0]/ar_subsample,
@@ -103,7 +128,8 @@ extract_subwin('../AEROSOL_RUN_RESULTS.hdf', 'ar',
 extract_metadata(src_cal,'metadata.txt')
 
 write_parms('ee_lndsr_parms.txt',
-            [ full_input_size[0], full_input_size[1],
+            [ 'SIXS_RUN_RESULTS.TXT',
+              full_input_size[0], full_input_size[1],
               srcwin[0], srcwin[1], srcwin[2], srcwin[3],
               aerosol_size[0], aerosol_size[1],
               utm_zone, ul_x, ul_y, pixel_size ] )
