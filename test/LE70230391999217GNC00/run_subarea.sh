13081e38b9b7bf809b6f6ab4f1b011f625c02c89
Frank Warmerdam <warmerdam@pobox.com>
2012-11-15 14:14:23 -0800

    use new target subarea with some cloud cover

--- test/LE70230391999217GNC00/run_subarea.sh
+++ test/LE70230391999217GNC00/run_subarea.sh
@@ -2,17 +2,17 @@
 
 lndsr lndsr.L71023039_03919990805.txt \
 	-debug \
-	-srcwin 2400 800 800 400 \
-	-check 2410 805  \
-	-check 2460 860  \
-	-check 2800 1000 \
-        -check 3050 970  \
+        -srcwin 1200 3600 800 400 \
+	-check 1210 3610  \
+	-check 1260 3860  \
+	-check 1600 3800 \
+        -check 1850 3970  \
 	|& tee subarea/lndsr.log
 
 ee_lndsr subarea \
-	-check 2410 805  \
-	-check 2460 860  \
-	-check 2800 1000 \
-        -check 3050 970  \
+	-check 1210 3610  \
+	-check 1260 3860  \
+	-check 1600 3800 \
+        -check 1850 3970  \
 	|& tee subarea/ee_lndsr.log
 
