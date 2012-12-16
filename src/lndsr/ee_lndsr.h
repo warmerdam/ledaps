fd0ebc1fa3bf74a69e2035d8914e871d63b8b528
Frank Warmerdam <warmerdam@pobox.com>
2012-12-15 17:14:51 -0800

    factor ee_lndsr_main() prototype out for easy external use

--- src/lndsr/ee_lndsr.h
+++ src/lndsr/ee_lndsr.h
@@ -23,6 +23,7 @@
 #include "error.h"
 #include "clouds.h"
 
+#include "ee_lndsr_main.h"
 #include "read_grib_tools.h"
 #include "sixs_runs.h"
 
@@ -41,28 +42,6 @@ Ar_gridcell_t *ee_build_ar_gridcell(Lut_t *lut,
                                     const float *anc_O3,
                                     const float *anc_dem);
 
-int ee_lndsr_main(
-    const char *metadata,
-    const char *sixs_filename,
-
-    int full_input_size_s, int full_input_size_l,
-    int input_offset_s, int input_offset_l,
-    int input_size_s, int input_size_l, int nband,
-    short *band1, short *band2, short *band3, 
-    short *band4, short *band5, short *band7, 
-    const short *thermal, const int8 *lndcal_QA, 
-    const float *anc_ATEMP, const char *csm_mask,
-    short *atmos_opacity, short *lndsr_QA,
-
-    int utm_zone, 
-    double ul_x, double ul_y, double pixel_size,
-
-    int ar_size_s, int ar_size_l,
-    const short *aerosol,
-    const float *anc_SP,
-    const float *anc_WV,
-    const float *anc_O3,
-    const float *anc_dem);
 
 bool ee_GetInputMeta(Input_meta_t *meta, const char *metadata);
 
---
+++ src/lndsr/ee_lndsr_main.h
@@ -0,0 +1,26 @@
+#ifndef EE_LNDSR_MAIN_H
+#define EE_LNDSR_MAIN_H
+int ee_lndsr_main(
+    const char *metadata,
+    const char *sixs_filename,
+
+    int full_input_size_s, int full_input_size_l,
+    int input_offset_s, int input_offset_l,
+    int input_size_s, int input_size_l, int nband,
+    short *band1, short *band2, short *band3,
+    short *band4, short *band5, short *band7,
+    const short *thermal, const int8 *lndcal_QA,
+    const float *anc_ATEMP, const char *csm_mask,
+    short *atmos_opacity, short *lndsr_QA,
+
+    int utm_zone,
+    double ul_x, double ul_y, double pixel_size,
+
+    int ar_size_s, int ar_size_l,
+    const short *aerosol,
+    const float *anc_SP,
+    const float *anc_WV,
+    const float *anc_O3,
+    const float *anc_dem);
+
+#endif /* EE_LNDSR_MAIN_H */
